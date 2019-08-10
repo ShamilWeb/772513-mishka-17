a498958ac922360cde5e9a663992f44290f371de
Шамиль <internetgggg@mail.ru>
2019-08-10 20:17:37 +0300

    Написан js для модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,30 @@ var navToggle = document.querySelector('.main-nav__toggle');
 navToggle.classList.remove('main-nav__toggle--nojs');
 navMain.classList.add('main-nav--closed');
 
-navToggle.addEventListener('click', function() {
-if (navMain.classList.contains('main-nav--closed')) {
-  navMain.classList.remove('main-nav--closed');
-  navMain.classList.add('main-nav--opened');
-} else {
-  navMain.classList.add('main-nav--closed');
-  navMain.classList.remove('main-nav--opened');
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+var openModal = document.querySelectorAll('.button--open-modal');
+var checkoutpopup = document.querySelector('.modal-cart');
+var modalOverlay = document.querySelector('.modal-overlay');
+
+for (i = 0; i < openModal.length; i++) {
+  openModal[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    checkoutpopup.classList.add("modal-cart--open");
+    modalOverlay.classList.add("modal-overlay-open");
+  });
 }
+
+modalOverlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  checkoutpopup.classList.remove("modal-cart--open");
+  modalOverlay.classList.remove("modal-overlay-open");
 });
